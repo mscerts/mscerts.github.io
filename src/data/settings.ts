f48ab95f414124488e3612794de438ac5af65d3e
Nighthouse <121154936+gitnighthouse@users.noreply.github.com>
2023-07-11 03:43:56 +0500

    v1

---
+++ src/data/settings.ts
@@ -0,0 +1,6 @@
+export const settings = {
+  site: 'https://mscerts.github.io',
+  name: 'Microsoft Certifications',
+  title: 'Microsoft Certifications',
+  description: 'Microsoft Certifications',
+}
---
+++ src/data/socials.ts
@@ -0,0 +1,17 @@
+export const socials = [
+  {
+    title: 'Discord',
+    icon: 'mdi:discord',
+    url: 'https://discord.gg/microsoft-certification-study-group-676990910176821270',
+  },
+  {
+    title: 'Reddit',
+    icon: 'mdi:reddit',
+    url: 'https://www.reddit.com/r/SCICertifications/',
+  },
+  {
+    title: 'GitHub',
+    icon: 'mdi:github',
+    url: 'https://github.com/mscerts/hub',
+  },
+]
